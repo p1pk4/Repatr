d54554383d0fb49c2f6e878c9fe28b9a59aaf794
Bugsy <Toxicbugsy@yandex.com>
2022-05-12 21:56:21 +0300

    Изменил поля: - Ограничил ввод заголовка max_length=15 - Заменил полу DateTimeField на DateField

--- news/models.py
+++ news/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class Articles(models.Model):
-    title = models.CharField('Название', max_length=50)  # max_length=15
-    hashtag = models.CharField('Хештег', max_length=20, default='Разное')  # default='Разное' - need delete
+    title = models.CharField('Название', max_length=15)
+    hashtag = models.CharField('Хештег', max_length=20)
     full_text = models.TextField('Текст')
-    date = models.DateTimeField('Дата публикации') #  Передавать текущую дату в поле
+    date = models.DateField('Дата публикации')
 
     class Meta:
-        verbose_name = 'Новость'  # это не новость, надо придумать слово для замены
+        verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
 
     def get_absolute_url(self):
